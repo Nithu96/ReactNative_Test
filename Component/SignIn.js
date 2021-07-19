4fe478b569fbe719f9fc9a75821b29fdd2cd7a00
Niwarthana Nihathamani <niwarthana.2016285@iit.ac.lk>
2021-07-20 03:08:36 +0530

    SignIn component validations

--- Component/SignIn.js
+++ Component/SignIn.js
@@ -2,6 +2,53 @@ import React,  { Component } from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity} from 'react-native';
 
 export default class SignIn extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+          emailtext: "",
+          emailError: "",
+          showPasswordError: true,
+          showEmailError: true,
+          isFirstLogin: true
+        }
+    
+    }
+
+    signInPressButton = () => {
+        const {showEmailError, showPasswordError} = this.state;
+
+        if (!showEmailError && !showPasswordError) {
+            alert("Login Success!")
+        }
+
+        this.setState({
+            isFirstLogin: false
+        })
+
+    }
+
+    validateEmail(email) {
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        const isValid = re.test(String(email).toLowerCase());
+        if (!isValid) {
+            this.setState({
+                showEmailError: true
+            })
+        } else {
+            this.setState({
+                showEmailError: false
+            })
+        }
+    }
+
+    checkAllowLogin() {
+        if (!(this.state.showEmailError && this.state.showPasswordError)) {
+            this.setState({
+                allowLogin: true
+            })
+        }
+    }
+
     render(){
         return(
            <View style = {styles.inputUi}>
@@ -11,9 +58,14 @@ export default class SignIn extends Component{
                       style={styles.inputText}
                       placeholder="your@address.net"
                       placeholderTextColor="black"
-                      autoCapitalize="none"  
+                      autoCapitalize="none" 
+                      onChangeText={(text) => {
+                        this.setState({ emailtext: text });
+                        this.validateEmail(text);
+                      }}  
                     />
                </View>
+               {this.state.showEmailError && !this.state.isFirstLogin ? (<Text style= {{color : 'red', marginLeft:20}}>{"Please Enter valid Email"}</Text>) : null}
                <View style ={ styles.inputView}>
                    <Text style ={styles.lableText}> Password </Text>
                     <TextInput
@@ -21,10 +73,24 @@ export default class SignIn extends Component{
                       placeholder="**********"
                       placeholderTextColor="black"
                       secureTextEntry
+                      onChangeText={(text) => {
+                        this.setState({ passwordtext: text });
+                        if (text != "") {
+                            this.setState({
+                                showPasswordError: false
+                            })
+                        } else {
+                            this.setState({
+                                showPasswordError: true
+                            })
+                        }
+                      }}  
                     />
                </View>
+               {this.state.showPasswordError && !this.state.isFirstLogin ? (<Text style= {{color : 'red', marginLeft:20}}>{"Password can not be empty"}</Text>) : null}
                <View alignItems='flex-end'>
-                   <TouchableOpacity style = {styles.loginBtn}>
+                   <TouchableOpacity style = {styles.loginBtn}
+                   onPress={this.signInPressButton} >
                        <Text style = {styles.loginText}>Sign-in</Text>
                    </TouchableOpacity>
                </View>
