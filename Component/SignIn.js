98c6f0d910f22ba8839a903aa628eafe8dfd1333
Niwarthana Nihathamani <niwarthana.2016285@iit.ac.lk>
2021-07-20 01:49:15 +0530

    SignIn component

---
+++ Component/SignIn.js
@@ -0,0 +1,12 @@
+import React,  { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class SignIn extends Component{
+    render(){
+        return(
+           <Text>SignIn</Text>
+        )
+    }
+}
+
+export {SignIn};
